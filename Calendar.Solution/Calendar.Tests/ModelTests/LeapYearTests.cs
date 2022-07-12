e732e97cf2d14a037523b593b94c8a7fdda2e5df
Derrak Richard <derrak@gmail.com>
2022-07-12 10:03:49 -0700

    added more tests
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>
    Co-authored-by: Anthony DiFalco <aodifalco@gmail.com>

--- Calendar.Solution/Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Solution/Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -12,5 +12,23 @@ namespace Calendar.Tests
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
     }
+    [TestMethod]
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+    }
+      [TestMethod]
+    public void IsLeapYear_MultiplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+      [TestMethod]
+    public void IsLeapYear_MultiplesOfFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+    }
   }
 }
